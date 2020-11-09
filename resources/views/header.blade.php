7202ae5a5e5a89dcfdb8f76c8dda2b4d3cb65e26
Bass El Hachem <basselhachem@kompo.io>
2020-11-09 12:52:48 -0500

    testing an issue with position fixed on mobile

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 <!-- IE compatibility - prefer edge -->
 <meta http-equiv="x-ua-compatible" content="IE=edge">
 
