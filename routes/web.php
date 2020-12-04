1f31a348d4fe4c98f4fa5ad87a482ce901c8563d
devyk <hortuk@yahoo.com>
2020-12-04 03:23:16 +0200

    fix: allow routes to be cached by laravel route:cache command

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,15 @@
 <?php
 
-use Kompo\Routing\Dispatcher;
+use Kompo\Http\Controllers\DispatchController;
+use Kompo\Http\Controllers\LocaleController;
 
-Route::middleware('web')->get('_kompo', function(){
-	return Dispatcher::dispatchConnection();
-})->name('_kompo');
+Route::middleware('web')->group(function() {
+    Route::get('_kompo', DispatchController::class)->name('_kompo');
+    Route::post('_kompo', DispatchController::class)->name('_kompo.post');
+    Route::put('_kompo', DispatchController::class)->name('_kompo.put');
+    Route::delete('_kompo', DispatchController::class)->name('_kompo.delete');
 
-Route::middleware('web')->post('_kompo', function(){
-	return Dispatcher::dispatchConnection();
-})->name('_kompo.post');
-
-Route::middleware('web')->put('_kompo', function(){
-	return Dispatcher::dispatchConnection();
-})->name('_kompo.put');
-
-Route::middleware('web')->delete('_kompo', function(){
-	return Dispatcher::dispatchConnection();
-})->name('_kompo.delete');
-
-
-Route::middleware('web')->group(function(){
-
-	/*
-	Route::get('kompo/keep-alive', function(){
-		return response(null, 204);
-	})->name('kompo.keep-alive');*/
-
-	if(count(config('kompo.locales')))
-
-		Route::get('set-locale/{locale?}', function ( $locale = 'en' ) {
-		    
-		    if(!array_key_exists($locale, config('kompo.locales') )) 
-		    	$locale = config('app.locale');
-
-		    \Cookie::queue('locale', $locale);
-		    
-		    return redirect()->back();
-		
-		})->name('setLocale');
-
+    if (count(config('kompo.locales'))) {
+        Route::get('set-locale/{locale?}', LocaleController::class)->name('setLocale');
+    }
 });
