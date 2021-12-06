6ddac76c8657764d392be310aded4bb3bfc5f367
basslhm <contact@kompo.io>
2021-12-06 11:07:14 -0500

    Add an icon

--- src/Usable/Svg.php
+++ src/Usable/Svg.php
@@ -159,6 +159,6 @@ class Svg extends Block
         'chevron-left' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>',
         'chevron-right' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>',
 
-
+        'plus' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>',
     ];
 }
