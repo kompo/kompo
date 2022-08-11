723240b465ff20bd6260a75ce5209566c2a86ba7
basslhm <contact@kompo.io>
2022-08-11 13:02:41 -0300

    Fix output format for flatpickr

--- src/Usable/Date.php
+++ src/Usable/Date.php
@@ -3,12 +3,14 @@
 namespace Kompo;
 
 use Kompo\Elements\Field;
+use Illuminate\Support\Carbon;
 
 class Date extends Field
 {
     public $vueComponent = 'Date';
 
     protected $dbFormat = 'Y-m-d';
+    protected $outputFormat = 'Y-m-d';
     protected $configFormatKey = 'default_date_format';
 
     protected function initialize($label)
@@ -54,6 +56,16 @@ class Date extends Field
         ]);
 
         return $this;
+    }
+
+    //If a date cast is added to the model's attribute, we should remove any reference to timezone before outputting
+    //Otherwise flatpickr changes it to a UTC date...
+    public function setOutput($value, $key)
+    {
+        if (!is_null($value)) {
+            $value = $value instanceof Carbon ? $value->format($this->outputFormat) : $value;
+            $this->value($value);
+        }
     }
 
     /** PROTECTED ***/
--- src/Usable/DateTime.php
+++ src/Usable/DateTime.php
@@ -2,11 +2,10 @@
 
 namespace Kompo;
 
-use Illuminate\Support\Carbon;
-
 class DateTime extends Date
 {
     protected $dbFormat = 'Y-m-d H:i:s';
+    protected $outputFormat = 'Y-m-d H:i';
     protected $configFormatKey = 'default_datetime_format';
 
     protected function initialize($label)
@@ -16,16 +15,5 @@ class DateTime extends Date
         $this->config([
             'enableTime' => true,
         ]);
-    }
-
-    //If a 'datetime' cast is added to the model's attribute,
-    //we should remove any reference to timezone before outputting
-    //Otherwise flatpickr changes it to a UTC date...
-    public function setOutput($value, $key)
-    {
-        if (!is_null($value)) {
-            $value = $value instanceof Carbon ? $value->format('Y-m-d H:i') : $value;
-            $this->value($value);
-        }
     }
 }
